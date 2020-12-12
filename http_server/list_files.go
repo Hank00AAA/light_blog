62621ca34771bae17031f6378315833b4692997e
Hank00AAA <154630877@qq.com>
2020-12-12 11:14:22 +0800

    special

--- http_server/list_files.go
+++ http_server/list_files.go
@@ -1,9 +1,7 @@
 package http_server
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"light_blog/constant"
 	"light_blog/markdown"
 
 	//"light_blog/markdown"
@@ -20,13 +18,13 @@ func list_files(ctx *gin.Context) {
 
 // createMarkDownList
 func createMarkDownFileList(m *markdown.MarkDownData) {
-	m.MarkdownAddTitle(1, "HanlShell")
+	m.MarkdownAddTitle(1, "小可爱下午英语考试加油～～～～")
 	m.MarkdownAddEmptyRow()
 
-	for _, v := range blogData.ListBlogFiles() {
-		m.MarkdownHttp(v, fmt.Sprintf("%v/getFile?" + constant.FileParamKey + "=%v", constant.GetURL(), v))
-		m.MarkdownAddEmptyRow()
-	}
+	//for _, v := range blogData.ListBlogFiles() {
+	//	m.MarkdownHttp(v, fmt.Sprintf("%v/getFile?" + constant.FileParamKey + "=%v", constant.GetURL(), v))
+	//	m.MarkdownAddEmptyRow()
+	//}
 
-	m.AddPic("miao", "https://github.com/Hank00AAA/light_blog/blob/main/pic/miao.jpeg?raw=true")
+	m.AddPic("miao", "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1607752834804&di=b416c06b3cf91c4e402041e0b8f302f1&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201803%2F08%2F20180308223406_zKyVN.jpeg")
 }
