14957ca0642bff99524dedb19c5bf59a48d63143
Hank00AAA <154630877@qq.com>
2020-12-13 14:34:54 +0800

    mosh

--- mosh/ubuntu_mosh.sh
+++ mosh/ubuntu_mosh.sh
@@ -4,7 +4,6 @@ sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:keithw/mosh
 sudo apt-get update
 sudo apt-get install mosh
-
 sudo iptables -I INPUT -p udp --dport 60001 -j ACCEPT 
 
 mosh_server
